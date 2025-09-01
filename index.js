8584caf2a623b326d7394c95b013ad96e295bb85
Sophien <bouderbala.s@gmail.com>
2025-09-01 10:24:45 +0200

    solution avec express

--- index.js
+++ index.js
@@ -3,16 +3,40 @@
 //  const server = http.createServer((req, res) => {
 //     res.end ("hello world!")
 //  })
-import { createServer } from 'node:http' //destructuration
-import 'dotenv/config'
+// import { createServer } from 'node:http' //destructuration
 
-const server = createServer((req, res) => {
-    res.end("Hello world!")
+import  express  from 'express'
+import 'dotenv/config'
+const app = express()
+
+app.get('/', (req, res) => {
+  res.end('GET : /')
+})
+
+app.post('/', (req, res) => {
+  res.end('POST : /')
+})
+
+app.get('/personne', (req, res) => {
+  res.end('GET : /personne')
+})
+
+app.get('/formation', (req, res) => {
+  res.end('GET : /formation')
 })
 
 const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
 
- server.listen(PORT, ()=>{
-    console.log(`adresse serveur : http://localhost:${PORT}`);
+// const server = createServer((req, res) => {
+//     res.end("Hello world!")
+// })
+
+// const PORT = process.env.PORT || 5000
+
+//  server.listen(PORT, ()=>{
+//     console.log(`adresse serveur : http://localhost:${PORT}`);
     
- })
+//  })
