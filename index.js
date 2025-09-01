84852cf93f2f0c0e2c556cc0232cce485980b2ef
Sophien <bouderbala.s@gmail.com>
2025-09-01 11:17:48 +0200

    middleware

--- index.js
+++ index.js
@@ -5,31 +5,64 @@
 //  })
 // import { createServer } from 'node:http' //destructuration
 
-import  express  from 'express'
+//expres js est un ensemble de middleware
+//un middleware est une fonction qui prend 3 parametres
+//middleware(req, res ,next) next permet d'appeler le middleware suivant
+
+
+
+
+// app.post('/', (req, res) => {
+  //   res.end('POST : /')
+  // })
+  
+  // app.get('/personne', (req, res) => {
+    //   res.end('GET : /personne')
+    // })
+    
+    // app.get('/formation', (req, res) => {
+      //   res.end('GET : /formation')
+      // })
+import express from 'express'
 import 'dotenv/config'
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.end('GET : /')
-})
+const m1 = (req, res, next) => {
+    console.log("Middleware : m1");
+    next()
+}
 
-app.post('/', (req, res) => {
-  res.end('POST : /')
-})
+const m2 = (req, res, next) => {
+    console.log("Middleware : m2");
+    next()
+}
 
-app.get('/personne', (req, res) => {
-  res.end('GET : /personne')
-})
+const m3 = (req, res) => {
+    console.log("Middleware : m3");
+}
 
-app.get('/formation', (req, res) => {
-  res.end('GET : /formation')
+app.get('/', (req, res, next) => {
+    console.log("GET : /")
+    res.end("GET : /")
+    next()
 })
+// }, [m2, m1])
+// app.use(m1)
 
+
+// app.use(m2)
+
+app.use([m1, m3, m2])
 const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
 
+app.listen(PORT, () => {
+    console.log(`Adresse serveur : http://localhost:${PORT}`);
+})
+// app.use(m1)
+
+
+// app.use(m2)
 // const server = createServer((req, res) => {
 //     res.end("Hello world!")
 // })
